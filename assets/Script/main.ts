fd21e9b9607540c6279bf33203fbbd24593d6578
xuan <824173706@qq.com>
2018-08-01 22:30:50 +0800

    Signed-off-by: xuan <824173706@qq.com>

--- assets/Script/main.ts
+++ assets/Script/main.ts
@@ -56,6 +56,29 @@ export default class main extends cc.Component {
         //         isBackGround = false;
         //     }
         // });
+        //适配解决方案
+        console.log("winSize: W: "+cc.director.getVisibleSize().width+" H: "+cc.director.getVisibleSize().height);
+        let _canvas:cc.Canvas = this.getComponent("cc.Canvas");
+
+        //设计分辨率比
+        let _rateR = _canvas.designResolution.height/_canvas.designResolution.width;
+        //显示分辨率比
+        let _rateV = cc.director.getVisibleSize().height/cc.director.getVisibleSize().width;
+        console.log("winSize: _rateR: "+_rateR+" _rateV: "+_rateV);
+        if(_rateV > _rateR)
+        {
+            _canvas.fitHeight = false;
+            _canvas.fitWidth = true;
+            console.log("winSize: fitWidth");
+        }
+        else
+        {
+            _canvas.fitHeight = true;
+            _canvas.fitWidth = false;
+            console.log("winSize: fitHeight");
+            
+        }
+        
     }
     start () {
         // init logic
